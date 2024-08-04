8cac3e2c1ceceda2249a4e9a15ca239ba2398b28
kimkihyeon <kimki1125@naver.com>
2024-08-04 12:05:02 +0900

    (chore) 뒤로가기 버튼 다른 화면과 일치

--- lib/View/SettingScreen.dart
+++ lib/View/SettingScreen.dart
@@ -114,6 +114,16 @@ class _SettingscreenState extends State<Settingscreen> {
       child: Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            highlightColor: Colors.grey,
+          ),
           title: const Text('Settings'),
           backgroundColor: Colors.black,
           foregroundColor: Colors.white,
